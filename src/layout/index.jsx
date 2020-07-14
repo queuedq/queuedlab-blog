fb76ebbb9a82af14df7ceaa68596f3d8d0a7c7e8
Dongkyu Han <queued37@gmail.com>
2020-07-14 17:18:14 +0900

    Initial styling

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import config from "../../data/SiteConfig";
+import "../sass/main.scss";
 import "./index.css";
 
 export default class MainLayout extends React.Component {
