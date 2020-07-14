7e722e45d4da888645947f8d458edb6577912bf0
Dongkyu Han <queued37@gmail.com>
2020-07-15 01:27:04 +0900

    Style post content

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
             resolve: "gatsby-remark-responsive-iframe"
           },
           "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers",
+          // "gatsby-remark-autolink-headers",
           "gatsby-remark-prismjs"
         ]
       }
--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -10,7 +10,7 @@ import SEO from "../components/SEO/SEO";
 import Footer from "../components/Footer/Footer";
 import config from "../../data/SiteConfig";
 import "./b16-tomorrow-dark.css";
-import "./post.css";
+import style from "./post.module.scss";
 
 export default class PostTemplate extends React.Component {
   render() {
@@ -31,8 +31,11 @@ export default class PostTemplate extends React.Component {
           <SEO postPath={slug} postNode={postNode} postSEO />
           <div>
             <h1>{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-            <div className="post-meta">
+            <div
+              className={style.postContent}
+              dangerouslySetInnerHTML={{ __html: postNode.html }}
+            />
+            <div className={style.postMeta}>
               <PostTags tags={post.tags} />
               <SocialLinks postPath={slug} postNode={postNode} />
             </div>
